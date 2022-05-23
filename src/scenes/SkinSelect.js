507c0aad05b74a5c9c3a9f380e528ba28b037177
Konstantinos Siskos <61664865+siskos-k@users.noreply.github.com>
2022-05-23 12:05:33 +0300

    Update SkinSelect.js
    optimized skin display size

--- src/scenes/SkinSelect.js
+++ src/scenes/SkinSelect.js
@@ -13,10 +13,10 @@ export default class SkinSelect extends Scene {
     this.selectedSkin = this.registry.get("selectedSkin");
     let sizeY = this.game.canvas.height;
     let sizeX = this.game.canvas.width;
-    this.add.image(sizeX/2, sizeY/2, "shopbg");
+    this.add.image(sizeX / 2, sizeY / 2, "shopbg");
 
-    let nextSkin = this.add.image(sizeX * 3/ 4, (sizeY * 2) / 3, "arrow_skin").setScale(0.4).setInteractive();
-    let previousSkin = this.add.image(sizeX/ 4, (sizeY * 2) / 3, "arrow_skin").setScale(0.4).setInteractive();
+    let nextSkin = this.add.image(sizeX * 3 / 4, (sizeY * 2) / 3, "arrow_skin").setScale(0.4).setInteractive();
+    let previousSkin = this.add.image(sizeX / 4, (sizeY * 2) / 3, "arrow_skin").setScale(0.4).setInteractive();
     previousSkin.flipX = true;
     let quasarButton = this.add.image(sizeX / 2, sizeY / 4, 'skinselect').setInteractive();
     let skinName = this.skinsArray[this.selectedSkin];
@@ -35,32 +35,34 @@ export default class SkinSelect extends Scene {
       }
     )
     nextSkin.on("pointerdown",
-    () => {
-      current_image.setVisible(false);
-      if (this.selectedSkin < this.skinsArray.length - 1) {
-        this.selectedSkin++;
-      } else {
-        this.selectedSkin = 0;
+      () => {
+        current_image.setVisible(false);
+        if (this.selectedSkin < this.skinsArray.length - 1) {
+          this.selectedSkin++;
+        } else {
+          this.selectedSkin = 0;
+        }
+        this.registry.set("selectedSkin", this.selectedSkin);
+        current_image = this.add.image(400, 400, this.skinsArray[this.selectedSkin]).setDisplaySize(120, 120);;
+        current_image.setVisible(true);
       }
-      this.registry.set("selectedSkin", this.selectedSkin);
-      current_image = this.add.image(400, 400, this.skinsArray[this.selectedSkin]).setDisplaySize(80, 80);;
-      current_image.setVisible(true);
-    }
     )
     previousSkin.on("pointerdown",
-    () => {
-      current_image.setVisible(false);
-      if (this.selectedSkin < this.skinsArray.length - 1) {
-        this.selectedSkin--;
-      } else {
-        this.selectedSkin = this.skinsArray.length - 1;
+      () => {
+        current_image.setVisible(false);
+        if (this.selectedSkin > 0) {
+          this.selectedSkin--;
+          console.log(this.selectedSkin);
+        } else {
+          this.selectedSkin = this.skinsArray.length - 1;
+          console.log(this.selectedSkin);
+        }
+        this.registry.set("selectedSkin", this.selectedSkin);
+        current_image = this.add.image(400, 400, this.skinsArray[this.selectedSkin]).setDisplaySize(120, 120);;
+        current_image.setVisible(true);
       }
-      this.registry.set("selectedSkin", this.selectedSkin);
-      current_image = this.add.image(400, 400, this.skinsArray[this.selectedSkin]).setDisplaySize(80, 80);;
-      current_image.setVisible(true);
-    }
-  )
-  
+    )
+
 
 
     //this.add.image(sizeX/2, sizeY/3, 'pog');
